a09085612f211f8524cdda19ad8088ef153b8aad
summer-sea <xiahaoyang_x@163.com>
2019-01-18 02:28:23 +0800

    新增商品中的选择类目功能，测试通过。

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemCaatController.java
@@ -0,0 +1,33 @@
+package com.taotao.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.pojo.EUTreeNode;
+import com.taotao.service.ItemCatService;
+
+/*
+ * 商品分类管理
+ * */
+@Controller
+@RequestMapping("/item/cat")
+public class ItemCaatController {
+	
+	@Autowired
+	private ItemCatService catService;
+	
+	
+	@RequestMapping("/list")
+	@ResponseBody
+	public List<EUTreeNode> getCatList(@RequestParam(value ="id",defaultValue="0")Long parentId){
+		List<EUTreeNode> list =catService.getCatList(parentId);
+		
+		return list;
+	}
+
+}
