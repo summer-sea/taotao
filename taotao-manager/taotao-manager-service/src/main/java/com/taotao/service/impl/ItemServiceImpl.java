5bfa89b4cdf621439d0d483142a042870be47571
summer-sea <xiahaoyang_x@163.com>
2019-01-17 23:59:28 +0800

    前后台调通，ssm框架整合完毕

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,34 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+	
+	@Autowired
+	private TbItemMapper itemMapper;
+
+	@Override
+	public TbItem getItemById(long itemId) {
+		
+		TbItemExample example = new TbItemExample();
+		com.taotao.pojo.TbItemExample.Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		 List<TbItem> list = itemMapper.selectByExample(example );
+		if(list !=null && list.size() >0){
+			TbItem item =list.get(0);
+			return item;
+		}
+		 // TODO Auto-generated method stub
+		return null;
+	}
+
+}
